c161abc1789c438727026b50178ecc5cc820aa9e
steerocea <109568273+steerocea@users.noreply.github.com>
2023-11-11 16:53:58 +1300

    title screen working + new assets

--- Final Flame/Scripts/title-screen.gd
+++ Final Flame/Scripts/title-screen.gd
@@ -1,7 +1,5 @@
 extends Control
 
-
-
 func _on_startbutton_pressed():
-	get_tree().change_scene("res://Scenes/World.tscn")
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 	pass # Replace with function body.
