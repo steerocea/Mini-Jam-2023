0441255e3a6d590d031f973daffc54c15d5d39ab
JebDudfield <110765820+JebDudfield@users.noreply.github.com>
2023-11-11 23:25:32 +1300

    Added scene-switching utility script

---
+++ Final Flame/Scripts/Autoloads/SceneDictionary.gd
@@ -0,0 +1,18 @@
+extends Node
+
+#Place for packed scenes to be stored, for scene switching.
+#Exists to avoid cyclic scene reference and avoid multiple preloading.
+
+var scene_dictionary:Dictionary = {
+	"Level_T": preload("res://Scenes/Levels/level_t.tscn"),
+	"title": preload("res://Scenes/title-screen.tscn")
+	}
+
+func change_scene(scene_key:String):
+	if(scene_dictionary.has(scene_key)):
+		get_tree().change_scene_to_packed(scene_dictionary[scene_key])
+	else:
+		var errStr = "Invalid scene key: " + scene_key + "\n";
+		for key in scene_dictionary.keys():
+			errStr += key + "\n"
+		push_error("Invalid scene key: " + scene_key )
